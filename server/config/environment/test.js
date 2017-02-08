32fcac5e132cf567c71ce8c8dad9f24b347a0219
Greg Thole <gthole@gmail.com>
2017-02-08 10:06:18 -0500

    Set dev/test configs to point to mongo layer

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/ttime-test'
+    uri: 'mongodb://mongo/ttime-test'
   },
   sequelize: {
     uri: 'sqlite://',
